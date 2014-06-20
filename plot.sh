6f0e136286e71e8b9af99b4fb9457166295c011d
Anders Sildnes <andsild@gmail.com>
2014-06-20 11:27:51 +0200

    plots are better, average is still wrong (division by ALL files instead of relevant)

--- plot.sh
+++ plot.sh
@@ -2,21 +2,45 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+declare -A solvers
+declare -A colors
+solvers[0]="gauss"
+    colors["${solvers[0]}"]="FFB801"
+solvers[1]="jacobi"
+    colors["${solvers[1]}"]="15A600"
+solvers[2]="sor"
+    colors["${solvers[2]}"]="5141FF"
+
+POINT_TYPE="1" #http://stelweb.asu.cas.cz/~nemeth/work/stuff/gnuplot/gnuplot-line-and-point-types-bw.png
+POINT_SIZE="2"
+
+
 cmd="plot"
-for file in $(find "${DIR}/data/" -type f -name "*average.dat")
+
+
+for solver in "${solvers[@]}"
 do
-    cmd="${cmd} \"${file}\" using 1 title \"test${file}\" with linespoints ,"
+    for file in $(find "${DIR}/build/output/${solver}" -type f -name "*average.dat")
+    do
+        cmd="${cmd} \"${file}\" using 1 title \"${solver}\" lc rgb \"#${colors["${solver}"]}\"  with lines   ,"
+    done
 done
 
-# cmd="${cmd%%,};"
+# cmd="${cmd%%,}"
 
-for file in $(find "${DIR}/data/" -regextype sed -regex ".*/[0-9].*\.dat")
+for solver in "${solvers[@]}"
 do
-    cmd="${cmd} \"${file}\" using 1 with points pt 7 lc rgb \"\#FFB801\" notitle,"
-    # break;
+    for file in $(find "${DIR}/build/output/${solver}" -regextype sed -regex ".*/[0-9].*\.dat")
+    do
+        cmd="${cmd} \"${file}\" using 1 with points pointtype ${POINT_TYPE} pointsize ${POINT_SIZE} lc rgb \"#${colors["${solver}"]}\" notitle,"
+    done
 done
 
-cmd="${cmd%%,}; set nomultiplot"
+cmd="${cmd%%,}"
 
-gnuplot -e "set terminal png; set output \"out.png\" ; ${cmd};"
+gnuplot -e \
+    "set terminal png; set output \"out.png\" ;
+     set xlabel \"iterations\" ; set ylabel \"error in central difference\" ;
+     set logscale y ;
+     ${cmd};"
 #echo "set terminal png; set output \"out.png\" ; ${cmd};"
--- src/file.cpp
+++ src/file.cpp
@@ -4,22 +4,18 @@
 #define PROJECT_DIR "PSB"
 #define DEFAULT_MODE      S_IRWXU | S_IRGRP |  S_IXGRP | S_IROTH | S_IXOTH
 
-//TODO: check for diagonal dominance
-//If you know that a matrix is diagonally dominant,
-//
-#include <iostream>
-#include <cmath>
 #include <algorithm>
-#include <vector>
-#include <stdio.h>
 #include <cmath>
+#include <errno.h>
+#include <iostream>
+#include <stdio.h>
 #include <string>
 #include <sys/stat.h>
+#include <vector>
 
 #include "./main.cpp"
 
 using namespace std;
-
 
 namespace file_IO
 {
@@ -41,26 +37,6 @@ class DirNotFound : public exception
         std::string msg_;
 };
 
-string get_path()
-{
-    char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    string sCwd = string(cwd);
-
-    size_t project_index = sCwd.find(PROJECT_DIR);
-    if (project_index!=string::npos)
-    {
-        size_t slash_index = sCwd.find_first_of("/", project_index);
-        if (slash_index!=string::npos)
-            return sCwd.substr(0, slash_index + 1);  //include slash
-    }
-    else {
-        return string("./");
-    }
-}
-
-
-
 void trimLeadingFileName(string &str)
 {
     size_t endpos = str.find_last_of("/");
@@ -81,7 +57,6 @@ void trimTrailingFilename(string &str)
     }
 }
 
-#include <errno.h>
 
 void mkdirp(const char* path, mode_t mode = DEFAULT_MODE) {
   // const cast for hack
@@ -150,9 +125,7 @@ vector<string> getFilesInFolder(string sDir)
     struct stat filestat;
     struct dirent *dirp;
 
-    string sFullDir = /* get_path() + */ sDir;
-
-    dp = opendir(sFullDir.c_str());
+    dp = opendir(sDir.c_str());
     if (dp == NULL)
     {
         throw DirNotFound(sDir);
@@ -162,7 +135,7 @@ vector<string> getFilesInFolder(string sDir)
 
     while ((dirp = readdir( dp )))
     {
-        string readFile = sFullDir + "/" + string(dirp->d_name);
+        string readFile = sDir + "/" + string(dirp->d_name);
 
         // Check for valid file(s)
         if (stat( readFile.c_str(), &filestat ))
