0962abc5affafd05963d99fab3b381e3baec4f83
Anders Sildnes <andsild@gmail.com>
2014-07-17 12:43:40 +0200

    some flow in solver

--- src/include/image_types.hpp
+++ src/include/image_types.hpp
@@ -1,6 +1,7 @@
 #ifndef IMAGE_TYPE_H
 #define  IMAGE_TYPE_H
 
+#include <string>
 #include <vector>
 
 #include "CImg.h"
@@ -9,10 +10,29 @@ typedef cimg_library::CImgList<double> imageList_fmt;
 typedef cimg_library::CImg<double> image_fmt;
 
 typedef void (*iterative_function)(const cimg_library::CImg<double> &arg1,
-        cimg_library::CImg<double> &arg2, double, int, double &arg3, double) ;
+                                    cimg_library::CImg<double> &arg2,
+                                    double, int, double &arg3, double) ;
 
-typedef void (*iterative_func)(const image_fmt &arg1,
-        image_fmt &arg2, double &arg3) ;
+typedef void (*iterative_func)(const image_fmt &arg1, image_fmt &arg2,
+                                double &arg3) ;
+
+class ImageException: public std::exception
+{
+    public:
+        explicit ImageException(const std::string& message):
+            msg_(message)
+         {}
+
+        virtual ~ImageException() throw (){}
+        virtual const char* what() const throw()
+        {
+            std::string ret = std::string("") + msg_;
+            return ret.c_str();
+        }
+    protected:
+        std::string msg_;
+};
+
 
 
 #endif
