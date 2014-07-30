2fbe789f7a6a6380d8a8861c90778ddef5912f6b
Anders Sildnes <andsild@gmail.com>
2014-07-30 12:53:07 +0200

    converted matlab to octave and have an imageprinter

---
+++ src/debug_tools/printImage.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import sys
+
+def printMe(filename):
+    im = Image.open(filename)
+    pixels = im.load()
+
+    for x in range(0, im.size[0]):
+        for y in range(0, im.size[1]):
+            print("%3d " % (pixels[x,y])) ,
+        print
+
+if not len(sys.argv) > 1:
+    print "Usage:", sys.argv[0], " <image_name>"
+else:
+    printMe(sys.argv[1])
